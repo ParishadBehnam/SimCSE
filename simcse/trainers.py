5e185fb323ca4471cf7cb58acd1c696c3216b262
vaibhavad <vaibhavadlakha95@gmail.com>
2024-02-20 19:28:10 +0000

    save method for instructor

--- simcse/trainers.py
+++ simcse/trainers.py
@@ -121,6 +121,17 @@ class CLTrainer(Trainer):
                     )
                 self.use_apex = True
 
+    def _save(self, output_dir: Optional[str] = None, state_dict=None):
+        # If we are executing this function, we are the process zero, so we don't check for that.
+        output_dir = output_dir if output_dir is not None else self.args.output_dir
+        os.makedirs(output_dir, exist_ok=True)
+        logger.info(f"Saving model checkpoint to {output_dir}")
+
+        self.model.save(output_dir)
+
+        # Good practice: save your training arguments together with the trained model
+        torch.save(self.args, os.path.join(output_dir, "training_args.bin"))
+
     def evaluate(
         self,
         eval_dataset: Optional[Dataset] = None,
