bdea6bb5018154bf5accf19bf1c941854f2dd582
Parishad <parishad.behnamghader@mila.quebec>
2024-02-20 10:40:53 -0500

    Fixed some problems. WIP

--- simcse/models.py
+++ simcse/models.py
@@ -15,6 +15,7 @@ from transformers.file_utils import (
     replace_return_docstrings,
 )
 from transformers.modeling_outputs import SequenceClassifierOutput, BaseModelOutputWithPoolingAndCrossAttentions
+from sentence_encoder.models import HFInstructor
 
 class MLPLayer(nn.Module):
     """
@@ -387,3 +388,196 @@ class RobertaForCL(RobertaPreTrainedModel):
                 mlm_input_ids=mlm_input_ids,
                 mlm_labels=mlm_labels,
             )
+
+
+
+class InstructorForCL(HFInstructor):
+    def __init__(
+        self,
+        model,
+        tokenizer,
+        pooling_mode: str,
+        max_length: int = 512,
+        temp: float = 0.05
+    ):
+        super().__init__(model, tokenizer, pooling_mode, max_length)
+        # self.encoder = super(HFInstructor)
+        self.sim = Similarity(temp=temp)
+        self.config = self.model.config
+
+    def forward(self, 
+                sentence_feature=None, 
+    # def forward(self,
+    #     input_ids=None,
+    #     attention_mask=None,
+    #     token_type_ids=None,
+    #     position_ids=None,
+                head_mask=None,
+                inputs_embeds=None,
+                labels=None,
+                output_attentions=None,
+                output_hidden_states=None,
+                return_dict=None,
+                sent_emb=False,
+                mlm_input_ids=None,
+                mlm_labels=None,
+                ):
+        if sent_emb:
+            raise ValueError("not implemented")
+            return sentemb_forward(self, self.roberta,
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+                position_ids=position_ids,
+                head_mask=head_mask,
+                inputs_embeds=inputs_embeds,
+                labels=labels,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+            )
+        else:
+            return self.cl_forward(
+                sentence_feature=sentence_feature,
+                head_mask=head_mask,
+                inputs_embeds=inputs_embeds,
+                labels=labels,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+                mlm_input_ids=mlm_input_ids,
+                mlm_labels=mlm_labels,
+            )
+    def cl_forward(cls,
+        sentence_feature=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        mlm_input_ids=None,
+        mlm_labels=None,
+    ):
+        input_ids = sentence_feature.get("input_ids")
+        attention_mask = sentence_feature.get("attention_mask")
+        token_type_ids = sentence_feature.get("token_type_ids")
+        position_ids = sentence_feature.get("position_ids")
+
+        return_dict = return_dict if return_dict is not None else cls.config.use_return_dict
+        ori_input_ids = input_ids
+        batch_size = input_ids.size(0)
+        # Number of sentences in one instance
+        # 2: pair instance; 3: pair instance with a hard negative
+        num_sent = input_ids.size(1)
+
+        mlm_outputs = None
+        # Flatten input for encoding
+        input_ids = input_ids.view((-1, input_ids.size(-1))) # (bs * num_sent, len)
+        attention_mask = attention_mask.view((-1, attention_mask.size(-1))) # (bs * num_sent len)
+        if token_type_ids is not None:
+            token_type_ids = token_type_ids.view((-1, token_type_ids.size(-1))) # (bs * num_sent, len)
+
+        # Get raw embeddings
+        # HFInstructor forward
+        if 'OLMo' in cls.model.__class__.__name__:
+            del sentence_feature['token_type_ids']
+        reps = cls.model(**sentence_feature)
+        if 'OLMo' in cls.model.__class__.__name__:
+            last_hidden_states = reps.hidden_states[-1]
+            output = cls.get_pooling(sentence_feature, last_hidden_states)
+        else:
+            output = cls.get_pooling(sentence_feature, reps.last_hidden_state)
+
+        # MLM auxiliary objective
+        if mlm_input_ids is not None:
+            raise ValueError("not implemented")
+            mlm_input_ids = mlm_input_ids.view((-1, mlm_input_ids.size(-1)))
+            mlm_outputs = encoder(
+                mlm_input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+                position_ids=position_ids,
+                head_mask=head_mask,
+                inputs_embeds=inputs_embeds,
+                output_attentions=output_attentions,
+                output_hidden_states=True if cls.model_args.pooler_type in ['avg_top2', 'avg_first_last'] else False,
+                return_dict=True,
+            )
+
+        # # Pooling
+        # pooler_output = cls.pooler(attention_mask, outputs)
+        output = output.view((batch_size, num_sent, output.size(-1))) # (bs, num_sent, hidden)
+
+        # # If using "cls", we add an extra MLP layer
+        # # (same as BERT's original implementation) over the representation.
+        # if cls.pooler_type == "cls":
+        #     pooler_output = cls.mlp(pooler_output)
+
+        # Separate representation
+        z1, z2 = output[:,0], output[:,1]
+
+        # Hard negative
+        if num_sent == 3:
+            z3 = output[:, 2]
+
+        # Gather all embeddings if using distributed training
+        if dist.is_initialized() and cls.training:
+            # Gather hard negative
+            if num_sent >= 3:
+                z3_list = [torch.zeros_like(z3) for _ in range(dist.get_world_size())]
+                dist.all_gather(tensor_list=z3_list, tensor=z3.contiguous())
+                z3_list[dist.get_rank()] = z3
+                z3 = torch.cat(z3_list, 0)
+
+            # Dummy vectors for allgather
+            z1_list = [torch.zeros_like(z1) for _ in range(dist.get_world_size())]
+            z2_list = [torch.zeros_like(z2) for _ in range(dist.get_world_size())]
+            # Allgather
+            dist.all_gather(tensor_list=z1_list, tensor=z1.contiguous())
+            dist.all_gather(tensor_list=z2_list, tensor=z2.contiguous())
+
+            # Since allgather results do not have gradients, we replace the
+            # current process's corresponding embeddings with original tensors
+            z1_list[dist.get_rank()] = z1
+            z2_list[dist.get_rank()] = z2
+            # Get full batch embeddings: (bs x N, hidden)
+            z1 = torch.cat(z1_list, 0)
+            z2 = torch.cat(z2_list, 0)
+
+        cos_sim = cls.sim(z1.unsqueeze(1), z2.unsqueeze(0))
+        # Hard negative
+        if num_sent >= 3:
+            z1_z3_cos = cls.sim(z1.unsqueeze(1), z3.unsqueeze(0))
+            cos_sim = torch.cat([cos_sim, z1_z3_cos], 1)
+
+        labels = torch.arange(cos_sim.size(0)).long().to(cls.device)
+        loss_fct = nn.CrossEntropyLoss()
+
+        # Calculate loss with hard negatives
+        if num_sent == 3:
+            # Note that weights are actually logits of weights
+            z3_weight = cls.model_args.hard_negative_weight
+            weights = torch.tensor(
+                [[0.0] * (cos_sim.size(-1) - z1_z3_cos.size(-1)) + [0.0] * i + [z3_weight] + [0.0] * (z1_z3_cos.size(-1) - i - 1) for i in range(z1_z3_cos.size(-1))]
+            ).to(cls.device)
+            cos_sim = cos_sim + weights
+
+        loss = loss_fct(cos_sim, labels)
+
+        # Calculate loss for MLM
+        if mlm_outputs is not None and mlm_labels is not None:
+            mlm_labels = mlm_labels.view(-1, mlm_labels.size(-1))
+            prediction_scores = cls.lm_head(mlm_outputs.last_hidden_state)
+            masked_lm_loss = loss_fct(prediction_scores.view(-1, cls.config.vocab_size), mlm_labels.view(-1))
+            loss = loss + cls.model_args.mlm_weight * masked_lm_loss
+
+        if not return_dict:
+            output = (cos_sim,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=cos_sim,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
